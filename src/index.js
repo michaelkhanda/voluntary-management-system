ad1274848ec3d1591a96291ea4b6327c8ac2d844
Michael Khanda <michaelkhanda006@gmail.com>
2023-07-25 11:53:42 +0300

    updated commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000; // Specify the port number you want to use
+const port = 3000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
