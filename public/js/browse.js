a21a4e55827e88ed5f3113453d3ccbc9cb409a45
Michael Khanda <michaelkhanda006@gmail.com>
2023-10-10 17:13:48 +0300

    updated commit

--- public/js/browse.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-	$(".browse-card").click(function () {
-		//window.location.replace("http://www.zigglor.xyz/works/volucare/opp.html");
-		window.location.href = "opp.html";
-	});
-});
